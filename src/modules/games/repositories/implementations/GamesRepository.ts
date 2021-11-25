6daf2265b50c6f8974f1349a00fdddb742882f88
SEdilson <edilson.silva00@hotmail.com>
2021-11-24 22:36:51 -0300

    Add queries to fix tests

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -7,24 +7,29 @@ import { IGamesRepository } from '../IGamesRepository';
 
 export class GamesRepository implements IGamesRepository {
   private repository: Repository<Game>;
+  private userRepository: Repository<User>;
 
   constructor() {
     this.repository = getRepository(Game);
+    this.userRepository = getRepository(User);
   }
 
   async findByTitleContaining(param: string): Promise<Game[]> {
     return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+      .createQueryBuilder('game')
+      .where('game.title ILIKE :param', { param: `%${param}%` })
+      .getMany();
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query('SELECT count(*) from games');
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
-    return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+    return this.userRepository
+      .createQueryBuilder('user')
+      .leftJoin('user.games', 'games')
+      .where('games.id = :id', { id })
+      .getMany()
   }
 }
